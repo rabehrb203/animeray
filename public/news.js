86942183005c7ab70213452b9250f6eb09815fc2
rabehrb203 <165825164+rabehrb203@users.noreply.github.com>
2024-06-17 18:33:22 +0100

    Update news.js

--- public/news.js
+++ public/news.js
@@ -1,39 +1,20 @@
-// pages/news.js
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import { Platform } from "react-native";
-
-const NewsPage = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    // التحقق مما إذا كان التطبيق مثبتًا على جهاز المستخدم
-    const isAppInstalled = () => {
-      // التحقق من نوع الجهاز
-      if (Platform.OS === "android") {
-        // التحقق من وجود التطبيق باستخدام الحزمة (package name)
-        const packageName = "com.animeray.news"; // تغييرها إلى اسم الحزمة الخاص بتطبيقك
-        return DeviceInfo.isAppInstalled(packageName);
-      } else {
-        // قم بالتحقق من وجود التطبيق بطريقة أخرى إذا لزم الأمر
-        // يمكنك استخدام واجهات برمجة تطبيقات أخرى أو مكتبات مناسبة لمنصة iOS
-        // على سبيل المثال: Linking.getInitialURL() في React Native
-        return false;
-      }
-    };
-    if (!isAppInstalled) {
-      // إذا لم يكن التطبيق مثبتًا، قم بتوجيه المستخدم إلى الرابط الخارجي
-      window.location.href = "رابط التطبيق على Google Play Store";
-    }
-  }, []);
-
+export default function Home() {
   return (
-    <div>
-      <h1>صفحة الأخبار</h1>
-      <p>محتوى الأخبار يتم عرضه هنا</p>
-    </div>
-  );
-};
+    <div className="container">
+      <Head>
+        <title>Anime Ray Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-export default NewsPage;
+      <main>
+        <Header title="Welcome to Anime Ray!" />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
