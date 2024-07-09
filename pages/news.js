b539e02f26877f9013dd672bc5478bb3bee13523
rabehrb203 <165825164+rabehrb203@users.noreply.github.com>
2024-07-09 11:38:22 +0100

    Update news.js

--- pages/news.js
+++ pages/news.js
@@ -1,1 +1,20 @@
-anime-ray.netlify.app
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Anime Ray Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to Anime Ray!" />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
